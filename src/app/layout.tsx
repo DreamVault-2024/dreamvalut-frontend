89d79a6dbd47144e440407230b7856ebb73e4055
leedawoon <acebasket42@gmail.com>
2024-03-21 08:16:33 +0900

    feat : 공용 컴포넌트 내비게이션 바 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/NavigationBar.tsx';
 
+// App 컴포넌트
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      
-    </main>
-  );
+  return <NavBar />;
 }
