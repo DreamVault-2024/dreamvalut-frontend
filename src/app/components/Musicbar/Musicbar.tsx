950efd507ef735daf21e41c81cf3f87676ee9c73
dongmin <dongmin11566@gmail.com>
2024-03-21 01:43:08 +0900

    design : Musicbar 아이콘 추가

--- src/app/components/Musicbar/Musicbar.tsx
+++ src/app/components/Musicbar/Musicbar.tsx
@@ -12,31 +12,11 @@ export default function MusicBar() {
   return (
     <div className="fixed bottom-[2%] left-[15%] w-[80%] h-[7%] rounded-md px-[2%] py-[0.5%] flex justify-between bg-gradient-to-r from-[#333333] from-20% via-[#7c7a47] via-50%  to-[#333333] to-90% shadow-lg">
       {/* 재생 컨트롤 버튼 */}
-      <div className="flex flex-row">
-        <img
-          src="https://via.placeholder.com/50"
-          alt="previous music btn"
-          width={50}
-          height={50}
-        />
-        <img
-          src="https://via.placeholder.com/50"
-          alt="play or stop btn"
-          width={50}
-          height={50}
-        />
-        <img
-          src="https://via.placeholder.com/50"
-          alt="next music btn"
-          width={50}
-          height={50}
-        />
-        <img
-          src="https://via.placeholder.com/50"
-          alt="replay btn"
-          width={50}
-          height={50}
-        />
+      <div className="flex flex-row py-[0.5%] space-x-3">
+        <img src="./pre-skip-btn/pre-skip-btn90.png" alt="previous music btn" />
+        <img src="./play-btn/play90.png" alt="play or stop btn" />
+        <img src="./skip-btn/skip-btn90.png" alt="next music btn" />
+        <img src="./replay-btn/replay-btn90.png" alt="replay btn" />
       </div>
       {/* 음악 정보 */}
       <div className="flex flex-row space-x-4">
