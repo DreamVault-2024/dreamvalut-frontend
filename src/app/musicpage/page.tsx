96bf07648bbd3635bf3a1f6d80c5ff6132997023
dongmin <dongmin11566@gmail.com>
2024-03-21 19:00:06 +0900

    design : 음악 페이지 블러 배경 퍼블리싱

--- src/app/musicpage/page.tsx
+++ src/app/musicpage/page.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable import/extensions */
 import NavBar from '../components/NavigationBar';
 
 export default function MusicPage() {
   return (
-    <div>
+    <div className="flex w-screen h-screen">
       <NavBar />
+      <img
+        src="https://i.ibb.co/hLxvjJG/1.jpg"
+        alt="1"
+        className="w-full h-full blur -z-10"
+      />
     </div>
   );
 }
