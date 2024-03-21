90abc3ed2c41c9bcef103164df0051c0d6b6bd86
j._.jinoo <jinoo0306@naver.com>
2024-03-22 08:38:25 +0900

    style : eslint, prettier 코드 스타일링 충돌 해결

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-unresolved
+
 import MusicBar from '../components/Musicbar/Musicbar';
 import NavigationBar from '../components/NavigationBar';
 
 function page() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-start">
+    <div className="w-screen h-full flex flex-col justify-start">
       <NavigationBar />
       <MusicBar />
-      <h1 className="text-2xl">인기 차트</h1>
+
+      <div className="w-full h-full ml-64 bg-gray-900">
+        <h1 className="mt-text-2xl">인기 차트</h1>
+      </div>
     </div>
   );
 }
