e04e630b8d09410bc3bc30f5fca5f8a21966ca29
leedawoon <acebasket42@gmail.com>
2024-03-21 17:48:45 +0900

    design: 로그인 페이지 배경 애니메이션 이미지 추가 및 z-index 수정

--- src/app/LoginPage.tsx
+++ src/app/LoginPage.tsx
@@ -18,24 +18,26 @@ const LogIn = () => {
 
   return (
     <>
-    <div className='tongtong_img'>
-
-    </div>
     <div className='flex justify-center items-center flex-col h-screen fade-in-box'>
-    <p className='text-4xl text-white mb-10'>Welcome To <span className='text-violet-600'>DreamVault.</span></p>
-    <p className='text-6xl text-white mb-16'>당신의 꿈을 열어보세요.</p>
-    <div className='flex LogInBtns justify-center items-center border-4 mb-4 p-2 w-1/5 border-purple-800 text-white rounded-xl hover-bg-'>
+    <p className='text-4xl text-white mb-10 z-10'>Welcome To <span className='text-violet-600 z-10'>DreamVault.</span></p>
+    <p className='text-6xl text-white mb-16 z-10'>당신의 꿈을 열어보세요.</p>
+    <div className='flex LogInBtns justify-center items-center mb-4 p-2 w-1/5 rounded-xl z-10'>
       <Image src={googlelogo} alt='구글로고' width={30} height={30} className='rounded-full'/>
       <button onClick={handleLogin} className='text-xl ml-10'>구글로 로그인</button>
     </div>
-    <div className='flex justify-center items-center border-4 border-purple-800 text-white mb-4 p-2 w-1/5 rounded-xl hover-bg-gradient-LoginBtn'>
+    <div className='flex LogInBtns justify-center items-center mb-4 p-2 w-1/5 rounded-xl z-10'>
     <Image src={kakaologo} alt='카카오톡로고' width={30} height={30} className='rounded-full'/>
     <button onClick={handleLogin} className='text-xl ml-10'>카카오톡으로 로그인</button>
   </div>
-  <div className='flex justify-center items-center border-4 border-purple-800 text-white mb-4 p-2 w-1/5 rounded-xl hover-bg-gradient-LoginBtn'>
+  <div className='flex LogInBtns justify-center items-center mb-4 p-2 w-1/5 rounded-xl z-10'>
   <Image src={naverlogo} alt='네이버로고' width={30} height={30} className='rounded-full' />
   <button onClick={handleLogin} className='text-xl ml-10'>네이버로 로그인</button>
 </div>
+<div className='z-1'>
+      <div className='wave -one'></div>
+      <div className='wave -two'></div>
+      <div className='wave -three'></div>
+    </div>
 </div>
 </>
   );
