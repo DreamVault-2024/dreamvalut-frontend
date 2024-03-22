994ba374ad168c07511de9d654316dd3fcb9572f
leedawoon <acebasket42@gmail.com>
2024-03-22 10:18:18 +0900

    index.css -> globals.css 로 내용 다 옮겼습니다. index.css 파일 삭제 이미지 호스팅(img 불러오는 방법 바꿈)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 // import MusicBar from './components/Musicbar/Musicbar';
 // import NavBar from './components/NavBar/NavigationBar';
-import LogIn from './LoginPage';
+import LogIn from './LogInPage/page';
 // App 컴포넌트
 export default function Home() {
   return (
