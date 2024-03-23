16c17ce882de5ef9e16f6e297a9dc6f7623cb089
dongmin <dongmin11566@gmail.com>
2024-03-23 16:43:03 +0900

    design : Musicbar 위치수정

--- src/app/components/Musicbar/Musicbar.tsx
+++ src/app/components/Musicbar/Musicbar.tsx
@@ -39,7 +39,7 @@ export default function MusicBar() {
   };
 
   return (
-    <div className="fixed bottom-[2%] left-[15%] w-[80%] h-[7%] rounded-md px-[2%] py-[0.5%] flex justify-between bg-gradient-to-r from-[#333333] from-20% via-[#7c7a47] via-50%  to-[#333333] to-90% shadow-lg">
+    <div className="fixed bottom-[1%] items-center w-[83%] h-[7%] rounded-md ml-[16%] px-[2%] py-[0.5%] flex justify-between bg-gradient-to-r from-[#333333] from-20% via-[#7c7a47] via-50%  to-[#333333] to-90% shadow-lg">
       {/* 재생 컨트롤 버튼 */}
       <div className="flex flex-row py-[0.5%] items-center">
         <ThemeProvider theme={theme}>
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable import/extensions */
+import MusicBar from '../components/Musicbar/Musicbar';
 import NavBar from '../components/NavBar/NavigationBar';
 
 export default function Mypage() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <MusicBar />
+      <div className="w-screen pl-[15%] bg-black h-screen"></div>
+    </>
   );
 }
