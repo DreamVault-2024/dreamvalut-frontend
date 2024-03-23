8792de562c41091fd5b3c7323343aa3fed51cf0e
leedawoon <acebasket42@gmail.com>
2024-03-23 21:10:17 +0900

    - 이미지를 호스팅하여 각 이미지를 변경 - 각 div를 button으로 변경

--- src/app/GenrePage/page.tsx
+++ src/app/GenrePage/page.tsx
@@ -4,102 +4,102 @@ import React from 'react';
 const GridComponent: React.FC = () => (
   <div className="flex justify-center h-screen">
     <div className="grid grid-cols-3 grid-rows-4 w-[30%] h-screen gap-[3%] p-[2%]">
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
           src="https://i.ibb.co/QYvMHz3/image.png"
           alt="image"
           className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">POP</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
           src="https://i.ibb.co/pKWSB7k/RNB.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">RnB</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
           src="https://i.ibb.co/QYvMHz3/image.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">Jazz</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/Fwj5D5s/image.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
-        <p className="text-zinc-50 mt-[8%] text-xl">K-POP</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+        <p className="text-zinc-50 mt-[8%] text-xl">Ballade</p>
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/nwnkLDJ/image.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">Classical</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/3cjmtvN/image.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">Rock</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/ZHJLj11/image.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">Hip-Hap</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/Bq4w6f9/Folk.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">Folk</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/HYCZ8KS/OST.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">OST</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/cgVFWw1/JPOP.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">J-POP</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/cgP2r1p/image.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">Musical</p>
-      </div>
-      <div className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-gray-900 rounded-xl">
+      </button>
+      <button className="flex flex-col items-center text-center justify-center border-2 border-purple-950 bg-zinc-900 rounded-xl">
         <img
-          src="https://i.ibb.co/QYvMHz3/image.png"
+          src="https://i.ibb.co/CvC2VdF/EDM.png"
           alt="image"
-          className="rounded-full"
+          className="w-[90%] h-[70%] rounded-full"
         />
         <p className="text-zinc-50 mt-[8%] text-xl">EDM</p>
-      </div>
+      </button>
     </div>
   </div>
 );
