07909af630d714e27d5e419a06f8c99fe09c9ce3
dongmin <dongmin11566@gmail.com>
2024-03-26 19:26:53 +0900

    design : 검색결과 페이지 상단 정보(곡정보,제작자,좋아요,재생)와 검색결과 리스트 너비 맞춤

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable import/extensions */
 import Divider from '@mui/material/Divider';
 import NavBar from '../components/NavBar/NavigationBar';
@@ -12,14 +14,37 @@ export default function SearchPage() {
           <p className="text-xl w-fit">에스파에 대한 검색 결과입니다.</p>
           <div className="flex flex-col bg-[#353535] h-fit rounded-xl w-full p-[2%] gap-4">
             <div className="flex flex-row justify-around items-center h-fit">
-              <p className="text-xl">곡정보</p>
+              <p className="text-xl w-[60%] text-center">곡정보</p>
               <p className="text-lg">제작자</p>
               <p className="text-lg">좋아요</p>
               <p className="text-lg">재생</p>
             </div>
             <Divider />
             <ul className="flex flex-col gap-4 h-fit">
-              <li>1</li>
+              <li className="flex flex-row justify-around items-center h-fit p-[1%]">
+                <div className="flex flex-row justify-between items-center w-[60%] gap-8">
+                  <div className="flex flex-col justify-center items-center w-fit gap-4">
+                    <img
+                      src="https://i.ibb.co/hLxvjJG/1.jpg"
+                      alt="cover"
+                      className="size-24 rounded-sm"
+                    />
+                    <p className="text-lg">title</p>
+                  </div>
+                  <p className="text-md items-center w-[80%]">
+                    "Cyclone Silence" merges the ethereal qualities of Ambient
+                    music with a more intense and forceful atmosphere. This
+                    track weaves together swirling synths and deep, pulsating
+                    beats, creating a soundscape that's both aggressive and
+                    enveloping. It's a journey through a storm of emotions,
+                    where the power of nature meets the tranquility of ambient
+                    soundscapes
+                  </p>
+                </div>
+                <p className="text-lg">aespa</p>
+                <p className="text-lg">1</p>
+                <p className="text-lg">1</p>
+              </li>
             </ul>
           </div>
         </div>
